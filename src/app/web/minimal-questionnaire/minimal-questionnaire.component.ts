63239450341f86f496d07ea962f5d772042e9de7
Dios Vo <51939323+diosvo@users.noreply.github.com>
2021-02-21 17:38:52 +0700

    Get answer nativeElement but can sync CSS related

--- src/app/web/minimal-questionnaire/minimal-questionnaire.component.ts
+++ src/app/web/minimal-questionnaire/minimal-questionnaire.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
+
 import { questionList } from 'src/app/configs/minimal-questionnaire/questionList';
 import { Question } from 'src/app/models/minimal-questionnaire';
 
@@ -12,18 +14,42 @@ export class MinimalQuestionnaireComponent implements OnInit {
   progressValue: number = 0
   questionList = questionList
 
+  //  static: true, answers => nativeElement undefined
+  @ViewChild('answer', { static: false }) answer?: ElementRef<HTMLDivElement>
+
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.increaseProgressValue();
+  }
+
+  ngAfterViewInit() {
+    console.log(this.answer);
+}
 
   get question(): Question {
     return this.questionList[this.currentQuestionIndex]
   }
 
-  onSelect(ans: HTMLDivElement): void { }
+  increaseProgressValue(): void {
+    this.progressValue = (100 * (this.currentQuestionIndex + 1)) / this.questionList.length
+  }
 
-  onPrev(): void {}
+  onSelect(answer: HTMLDivElement): void {
+    this.answer.nativeElement.childNodes.forEach((node: HTMLDivElement) => {
+      if (node.classList && node.classList.contains('selected')) {
+        node.classList.remove('selected')
+      }
+      answer.classList.add('selected');
+    });
+  }
 
-  onNext(): void {}
+  onPrev(): void {
+
+  }
+
+  onNext(): void {
+
+  }
 
 }
